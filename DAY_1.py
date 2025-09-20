f770655a3a6c274f2a9668bf779481e2b47dea52
abdullahgee0324 <abdullahgee0324@gmail.com>
2025-09-20 15:26:21 +0500

    DAY 1

---
+++ DAY_1.py
@@ -0,0 +1,45 @@
+This is what i learned today i know its a small thing but on journey to become a successful python developer
+# Numbers
+age = 25                # Integer
+height = 5.9            # Float
+complex_number = 3 + 2j # Complex
+
+# Text
+name = "Alice"          # String
+
+# Boolean
+is_student = True       # Boolean (True/False)
+
+# None
+nothing_here = None     # None means "nothing"
+
+# List (a sequence of items, can change later)
+fruits = ["apple", "banana", "cherry"]
+
+# Tuple (like a list, but cannot be changed)
+coordinates = (10, 20)
+
+# Set (collection of unique items, no duplicates)
+colors = {"red", "green", "blue", "red"}
+
+# Dictionary (key-value pairs)
+person = {
+    "name": "Alice",
+    "age": 25,
+    "is_student": True
+}
+
+print("Integer:", age)
+print("Float:", height)
+print("Complex number:", complex_number)
+
+print("String:", name)
+
+print("Boolean:", is_student)
+
+print("None type:", nothing_here)
+
+print("List:", fruits)
+print("Tuple:", coordinates)
+print("Set:", colors)
+print("Dictionary:", person)
